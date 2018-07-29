3841855c2a4fc5fb92a548ceb21c6cebe280910b
ndrwchn <ndrwchn@gmail.com>
2018-07-29 19:11:02 -0400

    update

--- main.py
+++ main.py
@@ -246,14 +246,14 @@ def digging():
 			# add some code to flipping here 
 			if stats_b == 'flipping buy':
 				logging.info('current trade_mode: %s, buy order stocks, flipping to buy submit first?' % config.first_submit)
-				config.first_submit = 'buy'
-				config.target_price = 'b1'
+				config.first_submit = 'sell'
+				config.target_price = 's1'
 				logging.info('trade_mode updated to %s' % config.first_submit)
 			
 			if stats_s == 'flipping sell':
 				logging.info('current trade_mode: %s, sell order stocks, fipping to sell submit first now?'  % config.first_submit)
-				config.first_submit = 'sell'
-				config.target_price = 's1'
+				config.first_submit = 'buy'
+				config.target_price = 'b1'
 				logging.info('trade_mode updated to %s' % config.first_submit)
 
 
