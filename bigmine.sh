667740aaa3c62df3ade208e11995d25b294333e1
Andrew Chen <ndrwchn@gmail.com>
2018-08-01 15:48:20 -0400

    bashmine

---
+++ bigmine.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# A sample Bash script, by Ryan
+cp ~/config.json.enoch.backup config.json
+python3 main.py
---
+++ smaillmine.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# A sample Bash script, by Ryan
+cp ~/config.json.96percent config.json
+python3 main.py
